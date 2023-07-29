132fa0df1f481a21b2c8afcb4453cabdd41ec59c
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-07-29 11:21:21 +0300

    feat: add config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  width: 640,
+  height: 640,
+  facingMode: "user",
+};
+
+export const dtConfig = {
+  inputResolution: { width: config.width, height: config.height },
+  scale: 0.8,
+};
